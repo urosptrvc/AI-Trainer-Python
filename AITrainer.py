694a99e94a90d7ac60f235d10ba83ea8b38799d2
Uros Petrovic <132246925+urosptrvc@users.noreply.github.com>
2024-11-15 18:54:00 +0100

    Version 1.1
    Full screen & color changes

--- AITrainer.py
+++ AITrainer.py
@@ -13,13 +13,16 @@ dir_right = 0
 dir_left = 0
 pTime = 0
 
+screen_width = 1920
+screen_height = 1080
+
 while True:
     success, img = cap.read()
     if not success:
         break
 
-    # Rezolucija slike
-    img = cv2.resize(img, (1280, 720))
+    # Skaliranje slike na fullscreen
+    img = cv2.resize(img, (screen_width, screen_height))
     img = detector.findPose(img, False)
     lmList = detector.findPosition(img, False)
 
@@ -59,29 +62,25 @@ while True:
                 dir_left = 0
 
         # Prikaz bara i procenta za desnu ruku
-        cv2.rectangle(img, (1100, 100), (1175, 650), color_right, 3)
-        cv2.rectangle(img, (1100, int(bar_right)), (1175, 650), color_right, cv2.FILLED)
-        cv2.putText(img, f'{int(per_right)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4, color_right, 4)
+        cv2.rectangle(img, (1800, 100), (1875, 650), color_right, 3)
+        cv2.rectangle(img, (1800, int(bar_right)), (1875, 650), color_right, cv2.FILLED)
+        cv2.putText(img, f'{int(per_right)}%', (1780, 75), cv2.FONT_HERSHEY_PLAIN, 3, color_right, 4)
 
         # Prikaz bara i procenta za levu ruku
         cv2.rectangle(img, (50, 100), (125, 650), color_left, 3)
         cv2.rectangle(img, (50, int(bar_left)), (125, 650), color_left, cv2.FILLED)
-        cv2.putText(img, f'{int(per_left)} %', (50, 75), cv2.FONT_HERSHEY_PLAIN, 4, color_left, 4)
+        cv2.putText(img, f'{int(per_left)}%', (30, 75), cv2.FONT_HERSHEY_PLAIN, 3, color_left, 4)
 
         # Prikaz broja ponavljanja za desnu ruku
-        cv2.putText(img, f'Right: {int(count_right)}', (900, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 5)
+        cv2.putText(img, f'Right: {int(count_right)}', (1570, 150), cv2.FONT_HERSHEY_PLAIN, 3, (51, 153, 255), 5)
 
         # Prikaz broja ponavljanja za levu ruku
-        cv2.putText(img, f'Left: {int(count_left)}', (50, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 5)
+        cv2.putText(img, f'Left: {int(count_left)}', (150, 150), cv2.FONT_HERSHEY_PLAIN, 3, (51, 153, 255), 5)
 
-    # FPS
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (50, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-
+    cv2.namedWindow("Fitness Tracker", cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty("Fitness Tracker", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.imshow("Fitness Tracker", img)
     # Prikaz slike
-    cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
